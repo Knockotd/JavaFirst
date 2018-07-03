4d49408a03fb474e41f256fdb9a62f2455f5ff9e
knockotd <anjeundeun@gmail.com>
2018-07-03 18:13:42 +0900

    2018.06.28

---
+++ my_practice/src/Practice1.java
@@ -0,0 +1,16 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("2+5="+2+5);
+System.out.println("2+5="+(2+5));
+
+System.out.println("12");
+System.out.println("1"+2);
+System.out.println(1+"2");
+System.out.println(3 * 4);
+System.out.println("1"+"2");
+	}
+
+}
