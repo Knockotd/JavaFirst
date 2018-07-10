77c19bc70fdfe8301e24760d1e074ed15a8e4ff4
knockotd <anjeundeun@gmail.com>
2018-07-10 18:21:37 +0900

    2018.07.10

--- simpleis-best.tistory.com_0710/src/ArrayLIST/MainEnum.java
+++ simpleis-best.tistory.com_0710/src/ArrayLIST/MainEnum.java
@@ -8,17 +8,14 @@ public class MainEnum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int gender = 1;
-		if (gender == 0) {
-
-		} else {
-
-		}
+		GENDER gender = GENDER.MAN;
+		//enum 으로 final함수를 묶을 때는 변수 자료형도 enum으로 묶어준 걸로 변형 시켜야 한다.
+		//값이 2가지 밖에 없어야 하는데 자료형이 int이면 여러개 가질 수도 있으니까.
 
 		final int MAN = 0;
 		final int WOMAN = 1;
 
-		if (gender == MAN) {
+		if (gender == GENDER.MAN) {
 		} else {
 		}
 		
